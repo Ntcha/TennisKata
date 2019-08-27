b69fecbdcaa389dffbfa5a6ed9179c3695fad810
Fab Actergal <fab@naego.be>
2019-08-27 13:01:16 +0200

    Refactor + enhance test harness

--- src/Score.py
+++ src/Score.py
@@ -3,28 +3,45 @@ class Score:
     def __init__(self):
         self.scale = ['Love', 'Fifteen', 'Thirty', 'Forty']
         self.deuce = 'Deuce'
+        self.min_value_to_win = 3
+        self.min_delta_to_win = 2
 
-    def announce(self, player_1_name, score_1, player_2_name, score_2):
+    def _order_players_by_score(self, player_1, player_2):
+        if player_1.score > player_2.score:
+            return player_1, player_2
+        return player_2, player_1
+
+    def _check_scores(self, score_1, score_2):
         if score_1 < 0 or score_2 < 0:
             raise ValueError("Score values should be greater than 0")
 
-        if score_1 == score_2 and score_1 >= 3:
+    def announce(self, player_1, player_2):
+        self._check_scores(player_1.score, player_2.score)
+
+        winner, looser = self._order_players_by_score(player_1, player_2)
+
+        if winner.score == looser.score and \
+                winner.score >= self.min_value_to_win:
             return self.deuce
 
-        if score_1 > score_2 and score_1 > 3:
-            return f'{player_1_name}, advantage'
+        if winner.score > looser.score \
+                and winner.score > self.min_value_to_win:
+            return f'{winner.name}, advantage'
 
-        if score_2 > score_1 and score_2 > 3:
-            return f'{player_2_name}, advantage'
-
-        return f'{self.scale[score_1]} - {self.scale[score_2]}'
+        return f'{player_1.name} : {self.scale[player_1.score]} -' \
+               f' {player_2.name} : {self.scale[player_2.score]}'
 
     def is_game_over(self, score_1, score_2):
         highest_score, lowest_score = score_1 > score_2 \
                                       and (score_1, score_2) \
                                       or (score_2, score_1)
 
-        if highest_score >= 3 and highest_score >= lowest_score + 2:
+        if highest_score >= self.min_value_to_win \
+                and highest_score >= lowest_score + self.min_delta_to_win:
             return True
 
         return False
+
+    def announce_winner(self, player_1, player_2):
+        if not self.is_game_over(player_1.score, player_2.score):
+            raise ValueError("Game is not over yet, continue to play !")
--- tst/test_score.py
+++ tst/test_score.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.Score import Score
+from src.Game import Player
 
 
 class Test_Score:
@@ -11,11 +12,11 @@ class Test_Score:
 
     @pytest.fixture
     def player_1(self):
-        return 'Fab'
+        return Player('Fab', 0)
 
     @pytest.fixture
     def player_2(self):
-        return 'Baf'
+        return Player('Baf', 0)
 
     def test_score_has_score_scale(self, score):
         assert(score.scale)
@@ -23,33 +24,37 @@ class Test_Score:
 
     def test_score_knows_standard_tennis_scores(self, score):
         assert(score.scale[0] == 'Love')
-        assert (score.scale[1] == 'Fifteen')
-        assert (score.scale[2] == 'Thirty')
-        assert (score.scale[3] == 'Forty')
+        assert(score.scale[1] == 'Fifteen')
+        assert(score.scale[2] == 'Thirty')
+        assert(score.scale[3] == 'Forty')
 
-    def test_score_can_announce(self, score, player_1, player_2):
-        assert(score.announce(player_1, 0, player_2, 0) == 'Love - Love')
-        assert(score.announce(player_1, 0, player_2, 3) == 'Love - Forty')
-        assert(score.announce(player_1, 1, player_2, 3) == 'Fifteen - Forty')
-        assert(score.announce(player_1, 2, player_2, 3) == 'Thirty - Forty')
-
-    def test_score_can_announce_deuce(self, score, player_1, player_2):
-        assert(score.announce(player_1, 3, player_2, 3) == 'Deuce')
-        assert (score.announce(player_1, 5, player_2, 5) == 'Deuce')
-
-    def test_score_can_announce_advantage(self, score, player_1, player_2):
-        assert(score.announce(player_1, 4, player_2, 3,)
-               == f'{player_1}, advantage')
-        assert (score.announce(player_1, 7, player_2, 8, )
-                == f'{player_2}, advantage')
+    @pytest.mark.parametrize("score, player_1_name, player_1_score, \
+                             player_2_name, player_2_score, expected", [
+        (Score(), 'Fab', 0, 'Baf', 0, 'Fab : Love - Baf : Love'),
+        (Score(), 'Fab', 0, 'Baf', 1, 'Fab : Love - Baf : Fifteen'),
+        (Score(), 'Fab', 0, 'Baf', 2, 'Fab : Love - Baf : Thirty'),
+        (Score(), 'Fab', 0, 'Baf', 3, 'Fab : Love - Baf : Forty'),
+        (Score(), 'Fab', 1, 'Baf', 0, 'Fab : Fifteen - Baf : Love'),
+        (Score(), 'Fab', 2, 'Baf', 0, 'Fab : Thirty - Baf : Love'),
+        (Score(), 'Fab', 3, 'Baf', 0, 'Fab : Forty - Baf : Love'),
+        (Score(), 'Fab', 3, 'Baf', 3, 'Deuce'),
+        (Score(), 'Fab', 4, 'Baf', 4, 'Deuce'),
+        (Score(), 'Fab', 5, 'Baf', 4, 'Fab, advantage'),
+        (Score(), 'Fab', 5, 'Baf', 6, 'Baf, advantage')
+    ])
+    def test_score_can_announce(self, score, player_1_name, player_1_score,
+                                player_2_name, player_2_score, expected):
+        player_1 = Player(player_1_name, player_1_score)
+        player_2 = Player(player_2_name, player_2_score)
+        assert(score.announce(player_1, player_2) == expected)
 
     def test_game_over(self, score):
         assert(not score.is_game_over(0, 0))
         assert(score.is_game_over(0, 3))
 
     def test_game_over_advantage(self, score):
-        assert (not score.is_game_over(3, 3))
-        assert (not score.is_game_over(4, 3))
-        assert (not score.is_game_over(4, 4))
-        assert (score.is_game_over(4, 6))
-        assert (score.is_game_over(8, 6))
+        assert(not score.is_game_over(3, 3))
+        assert(not score.is_game_over(4, 3))
+        assert(not score.is_game_over(4, 4))
+        assert(score.is_game_over(4, 6))
+        assert(score.is_game_over(8, 6))
