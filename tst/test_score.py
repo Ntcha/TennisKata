a700a5068514840a374ef328c5c956dcefee84d5
Fab Actergal <fab@naego.be>
2019-08-27 12:23:19 +0200

    Refactor to comply with flake, enhance test harness

--- tst/test_score.py
+++ tst/test_score.py
@@ -38,14 +38,18 @@ class Test_Score:
         assert (score.announce(player_1, 5, player_2, 5) == 'Deuce')
 
     def test_score_can_announce_advantage(self, score, player_1, player_2):
-        assert(score.announce(player_1, 4, player_2, 3,) == f'{player_1}, advantage')
+        assert(score.announce(player_1, 4, player_2, 3,)
+               == f'{player_1}, advantage')
+        assert (score.announce(player_1, 7, player_2, 8, )
+                == f'{player_2}, advantage')
 
     def test_game_over(self, score):
         assert(not score.is_game_over(0, 0))
         assert(score.is_game_over(0, 3))
 
-    def test_game_over_deuce(self, score):
+    def test_game_over_advantage(self, score):
         assert (not score.is_game_over(3, 3))
         assert (not score.is_game_over(4, 3))
         assert (not score.is_game_over(4, 4))
         assert (score.is_game_over(4, 6))
+        assert (score.is_game_over(8, 6))
