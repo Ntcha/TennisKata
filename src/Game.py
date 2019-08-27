bee5b051ebf7ad47b8d15de9ebb8289b7727405b
Fab Actergal <fab@naego.be>
2019-08-27 11:37:29 +0200

    Add player scores

--- src/Game.py
+++ src/Game.py
@@ -1,4 +1,10 @@
+from collections import namedtuple
+
+Player = namedtuple('Player', ['name', 'score'])
+
+
 class Game:
 
     def __init__(self, first_player_name, second_player_name):
-        self.players = [first_player_name, second_player_name]
+        self.players = [Player(first_player_name, 0),
+                        Player(second_player_name, 0)]
--- tst/test_game.py
+++ tst/test_game.py
@@ -1,17 +1,29 @@
+import pytest
 from src.Game import Game
 
+
 class Test_Game:
+    @pytest.fixture
+    def player_1(self):
+        return 'Fab'
+
+    @pytest.fixture
+    def player_2(self):
+        return 'baf'
 
     def test_game_has_2_players(self):
         game = Game(None, None)
 
         assert(len(game.players) == 2)
 
-    def test_game_retains_user_names(self):
-        player_1 = 'Fab'
-        player_2 = 'Baf'
-
+    def test_game_retains_user_names(self, player_1, player_2):
         game = Game(player_1, player_2)
 
-        assert(game.players[0] == player_1)
-        assert (game.players[1] == player_2)
+        assert(game.players[0].name == player_1)
+        assert (game.players[1].name == player_2)
+
+    def test_game_player_have_scores(self, player_1, player_2):
+        game = Game(player_1, player_2)
+
+        assert(game.players[0].score == 0)
+        assert (game.players[1].score == 0)
